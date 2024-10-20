6211a7e2304bc28e7c455f4c405b8366fc638928
MasterHiggins <151066454+MasterHiggins@users.noreply.github.com>
2024-10-20 20:08:05 -0230

    The service class for Aircraft

---
+++ src/main/java/com/keyin/aircraft/AircraftService.java
@@ -0,0 +1,41 @@
+package com.keyin.aircraft;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AircraftService {
+
+    @Autowired
+    private AircraftRepository aircraftRepository;
+
+    public List<Aircraft> findAllAircrafts() {
+        return (List<Aircraft>) aircraftRepository.findAll();
+    }
+
+    public Aircraft getAircraftById(Long id) {
+        return aircraftRepository.findById(id).orElse(null);
+    }
+
+    public Aircraft addAircraft(Aircraft aircraft) {
+        return aircraftRepository.save(aircraft);
+    }
+
+    public Aircraft updateAircraft(Long id, Aircraft updatedAircraft) {
+        Optional<Aircraft> aircraftToUpdateOptional = aircraftRepository.findById(id);
+        if (aircraftToUpdateOptional.isPresent()) {
+            Aircraft aircraftToUpdate = aircraftToUpdateOptional.get();
+            aircraftToUpdate.setModel(updatedAircraft.getModel());
+            aircraftToUpdate.setCapacity(updatedAircraft.getCapacity());
+            return aircraftRepository.save(aircraftToUpdate);
+        }
+        return null;
+    }
+
+    public void deleteAircraft(Long id) {
+        aircraftRepository.deleteById(id);
+    }
+}
