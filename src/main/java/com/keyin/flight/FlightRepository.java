dce330d9d7318b7f777e687251421674d64c82c1
Maz Radwan <maz.radwan@keyin.com>
2024-10-20 10:50:36 -0230

    created flight repository

--- src/main/java/com/keyin/flight/FlightRepository.java
+++ src/main/java/com/keyin/flight/FlightRepository.java
@@ -1,4 +1,10 @@
 package com.keyin.flight;
 
-public interface FlightRepository {
-}
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+    public interface FlightRepository extends CrudRepository<Flight, Long>{
+    }
+
